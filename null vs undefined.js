b0419240f906e001ac90a7a039abf4f91a7a121b
Shakawat-dev <shakawathridoy7@gmail.com>
2020-07-03 00:59:25 +0600

    null vs undefined done

---
+++ null vs undefined.js
@@ -0,0 +1,21 @@
+//Undefined//
+let pakhi;
+function add(num1, num2) {
+  console.log(num1 + num2); // nothing return
+  return; // nothing return
+}
+function add(num1, num2) {
+  console.log(num1, num2);
+}
+const result = add(33); // second value show undefined.....nothing can pass
+console.log(result);
+
+const premik = { name: "name dude", phone: 12344 };
+console.log(premik.gf); //undefined
+
+let fun = undefined; //undefined
+console.log(fun);
+
+null; // not exited
+let ages = [2, 3, 11];
+console.log(ages[11]);
